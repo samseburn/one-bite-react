4db8084ead321c0d23a02f01d52da6ef80722f2b
samseburn <90seok_0@naver.com>
2022-11-29 19:17:50 +0900

    🎉 리액트 실습 프로젝트 생성

---
+++ react-basic/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  let name = "고석영";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>안녕 리액트, {name}</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
