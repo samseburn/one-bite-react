6c7610e702a6e67c437b0edfa1418458e1396177
samseburn <90seok_0@naver.com>
2022-11-30 20:16:01 +0900

    React 기본 - 사용자 입력 처리 및 useRef로 DOM 조작

--- simple-diary/src/DiaryEditor.js
+++ simple-diary/src/DiaryEditor.js
@@ -1,5 +1,79 @@
+import React, { useRef, useState } from 'react';
+
 const DiaryEditor = () => {
-	return <div className="DiaryEditor"></div>;
+	const authorInput = useRef();
+	const contentInput = useRef();
+
+	const [state, setState] = useState({
+		author: '',
+		content: '',
+		emotion: 1,
+	});
+
+	// state 상태 값 변경
+	const handleChangeState = e => {
+		setState({
+			...state,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	// 현재 state 상태 출력
+	const handleSubmit = e => {
+		// console.log(state);
+
+		if (state.author.length < 1) {
+			// focus
+			authorInput.current.focus();
+			return;
+		}
+
+		if (state.content.length < 5) {
+			// focus
+			contentInput.current.focus();
+			return;
+		}
+		alert('저장 성공');
+	};
+
+	return (
+		<div className="DiaryEditor">
+			<h2>오늘의 일기</h2>
+			<div>
+				<input
+					ref={authorInput}
+					name="author"
+					type="text"
+					value={state.author}
+					onChange={handleChangeState}
+				/>
+			</div>
+			<div>
+				<textarea
+					name="content"
+					value={state.content}
+					onChange={handleChangeState}
+				/>
+			</div>
+			<div>
+				<select
+					ref={contentInput}
+					name="emotion"
+					value={state.emotion}
+					onChange={handleChangeState}
+				>
+					<option value={1}>1</option>
+					<option value={2}>2</option>
+					<option value={3}>3</option>
+					<option value={4}>4</option>
+					<option value={5}>5</option>
+				</select>
+			</div>
+			<div>
+				<button onClick={handleSubmit}>일기 저장하기</button>
+			</div>
+		</div>
+	);
 };
 
 export default DiaryEditor;
