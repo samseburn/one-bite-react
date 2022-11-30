352e4b86d880a2b9bb7414f4355009af5907e3b3
samseburn <90seok_0@naver.com>
2022-11-30 21:03:30 +0900

    React 기본 - DiaryList에서 DiaryItem 분리

--- simple-diary/src/DiaryList.js
+++ simple-diary/src/DiaryList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import DiaryItem from './DiaryItem';
 
 const DiaryList = props => {
 	const { diaryList } = props;
@@ -9,12 +9,7 @@ const DiaryList = props => {
 			<h4>{diaryList.length}개의 일기가 있습니다.</h4>
 			<div>
 				{diaryList.map(item => (
-					<div key={item.id}>
-						<div>작성자: {item.author}</div>
-						<div>일기: {item.content}</div>
-						<div>감정 점수: {item.emotion}</div>
-						<div>작성 시간(ms): {item.created_date}</div>
-					</div>
+					<DiaryItem key={item.id} {...item} />
 				))}
 			</div>
 		</div>
