104debb506c996cdc4ff369746d85ebab375c109
samseburn <90seok_0@naver.com>
2022-12-04 18:15:13 +0900

    React 기본 - API 호출하기

--- simple-diary/src/App.js
+++ simple-diary/src/App.js
@@ -1,14 +1,37 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 import Lifecycle from './Lifecycle';
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 	// 일기 데이터 저장할 빈 배열 생성
 	const [data, setData] = useState([]);
 
 	const dataId = useRef(1);
+
+	const getData = async () => {
+		const res = await fetch(
+			'https://jsonplaceholder.typicode.com/comments',
+		).then(res => res.json());
+
+		const initData = res.slice(0, 20).map(item => {
+			return {
+				author: item.email,
+				content: item.body,
+				emotion: Math.floor(Math.random() * 5) + 1,
+				created_date: new Date().getTime(),
+				id: dataId.current++,
+			};
+		});
+		console.log(initData);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
 
 	const onCreate = (author, content, emotion) => {
 		const created_date = new Date().getTime();
@@ -40,7 +63,7 @@ function App() {
 
 	return (
 		<div className="App">
-			<Lifecycle />
+			{/* <Lifecycle /> */}
 			<DiaryEditor onCreate={onCreate} />
 			<DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
 		</div>
