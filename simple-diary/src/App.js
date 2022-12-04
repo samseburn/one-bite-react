625e8781d7dbf88a33410238eddc5ea013e46c41
samseburn <90seok_0@naver.com>
2022-12-04 21:44:27 +0900

    React 기본 - 최적화 1 :: useMemo

--- simple-diary/src/App.js
+++ simple-diary/src/App.js
@@ -1,10 +1,7 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
-import Lifecycle from './Lifecycle';
-
-// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 	// 일기 데이터 저장할 빈 배열 생성
@@ -26,7 +23,7 @@ function App() {
 				id: dataId.current++,
 			};
 		});
-		console.log(initData);
+		// console.log(initData);
 		setData(initData);
 	};
 
@@ -48,7 +45,7 @@ function App() {
 	};
 
 	const onRemove = targetId => {
-		console.log(`${targetId}가 삭제되었습니다.`);
+		// console.log(`${targetId}가 삭제되었습니다.`);
 		const newDirayList = data.filter(item => item.id !== targetId);
 		setData(newDirayList);
 	}; // targetId --> DirayItem에서 id
@@ -62,10 +59,34 @@ function App() {
 		);
 	};
 
+	// 감정 점수를 필터링하여 카운팅
+	// useMemo를 사용하여 함수를 최적화하게 되며,
+	// 함수는 더이상 함수가 아니고 값을 반환하게 됨 여기에서는 객체 {goodCout, badCount, goodRatio}
+	const getDiaryAnalysis = useMemo(() => {
+		// console.log('일기 분석 시작');
+
+		// 3점을 기준으로 감정 점수 분류
+		const goodCount = data.filter(item => item.emotion >= 3).length;
+		const badCount = data.length - goodCount;
+
+		// 좋은 감정 점수 비율 구하기
+		const goodRatio = (goodCount / data.length) * 100;
+
+		// 객체로 리턴
+		return { goodCount, badCount, goodRatio };
+	}, [data.length]); // 두번째 인자로 받은 값이 변화할 때만 첫 번째 인자로 받은 콜백함수가 실행 즉, 리렌더링 진행
+
+	// 객체 비구조화 할당으로 지역 함수의 리턴값 변수에 저장
+	// 함수가 아닌 값을 리턴받게 되므로 값으로 사용해야 함
+	const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
 	return (
 		<div className="App">
-			{/* <Lifecycle /> */}
 			<DiaryEditor onCreate={onCreate} />
+			<div>전체 일기 : {data.length}</div>
+			<div>기분 좋은 일기 개수: {goodCount}</div>
+			<div>기분 나쁜 일기 개수: {badCount}</div>
+			<div>기분 좋은 일기 비율: {goodRatio}</div>
 			<DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
 		</div>
 	);
