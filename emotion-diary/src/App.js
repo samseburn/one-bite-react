6a6dd9f68a5b8413892615903bbab0c693872aab
samseburn <90seok_0@naver.com>
2022-12-08 21:28:35 +0900

    React 실전 - Home :: 페이지 구현 및 스타일링

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -43,8 +43,19 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+	{ id: 1, emotion: 1, content: '오늘의 일기 1번', date: 1670482354623 },
+	{ id: 2, emotion: 2, content: '오늘의 일기 2번', date: 1670482354624 },
+	{ id: 3, emotion: 3, content: '오늘의 일기 3번', date: 1670482354625 },
+	{ id: 4, emotion: 4, content: '오늘의 일기 4번', date: 1670482354626 },
+	{ id: 5, emotion: 5, content: '오늘의 일기 5번', date: 1670482354627 },
+	{ id: 6, emotion: 2, content: '오늘의 일기 6번', date: 1570482354617 },
+];
+
 function App() {
-	const [data, dispatch] = useReducer(reducer, []);
+	const [data, dispatch] = useReducer(reducer, dummyData);
+
+	// console.log(new Date().getTime());
 
 	const dataId = useRef(0);
 
@@ -94,7 +105,7 @@ function App() {
 							<Route path="/edit" element={<Edit />} />
 							<Route path="/diary/:id" element={<Diary />} />
 						</Routes>
-						<RouteTest />
+						{/* <RouteTest /> */}
 					</div>
 				</BrowserRouter>
 			</DiaryDispatchContext.Provider>
--- emotion-diary/src/pages/Home.js
+++ emotion-diary/src/pages/Home.js
@@ -1,10 +1,75 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { DiaryStateContext } from '../App';
+
+// COMPONENTS
+import MyHeader from '../components/MyHeader';
+import MyButton from '../components/MyButton';
+import DiaryList from '../components/DiaryList';
+
+// HEADER
+// Mount 되는 동시에 왼쪽 오른쪽 버튼 구현
+//  1. 현재 시점 표시
+//  2. 왼쪽 버튼 구현
+//  3. 오른쪽 버튼 구현
 
 const Home = () => {
+	const diaryList = useContext(DiaryStateContext);
+	const [data, setData] = useState([]);
+	const [curDate, setCurDate] = useState(new Date());
+	const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+	useEffect(() => {
+		if (diaryList.length >= 1) {
+			const firstDay = new Date(
+				curDate.getFullYear(),
+				curDate.getMonth(),
+				1,
+			).getTime();
+
+			const lastDay = new Date(
+				curDate.getFullYear(),
+				curDate.getMonth() + 1,
+				0,
+			).getTime();
+
+			setData(
+				diaryList.filter(item => firstDay <= item.date && item.date <= lastDay),
+			);
+		}
+	}, [diaryList, curDate]);
+	// CHECK :: 필터 결과값 데이터 확인
+	useEffect(() => {
+		console.log(data);
+	}, [data]);
+
+	const increaseMonth = () => {
+		setCurDate(
+			new Date(
+				curDate.getFullYear(),
+				curDate.getMonth() + 1,
+				curDate.getDate(),
+			),
+		);
+	};
+
+	const decreaseMonth = () => {
+		setCurDate(
+			new Date(
+				curDate.getFullYear(),
+				curDate.getMonth() - 1,
+				curDate.getDate(),
+			),
+		);
+	};
+
 	return (
 		<div>
-			<h1>Home</h1>
-			<p>이곳은 홈 입니다. </p>
+			<MyHeader
+				headText={headText}
+				leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
+				rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
+			/>
+			<DiaryList diaryList={data} />
 		</div>
 	);
 };
