990c7104da02995c1ecfa0bf4c53d09bd0afda31
samseburn <90seok_0@naver.com>
2022-12-09 16:35:23 +0900

    React 실전 - Edit :: 페이지 구현 및 스타일링

--- emotion-diary/src/components/DiaryEditor.js
+++ emotion-diary/src/components/DiaryEditor.js
@@ -1,5 +1,5 @@
-import { useContext, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useContext, useEffect, useRef, useState } from 'react';
+import { useFetcher, useNavigate } from 'react-router-dom';
 import { DiaryDispatchContext } from '../App';
 
 import MyHeader from './MyHeader';
@@ -49,13 +49,13 @@ const getStringDate = date => {
 	return `${year}-${month}-${day}`;
 };
 
-const DiaryEditor = () => {
+const DiaryEditor = ({ isEdit, originData }) => {
 	const contentRef = useRef();
 	const [content, setContent] = useState('');
 	const [emotion, setEmotion] = useState(3);
 	const [date, setDate] = useState(getStringDate(new Date()));
 
-	const { onCreate } = useContext(DiaryDispatchContext);
+	const { onCreate, onEdit } = useContext(DiaryDispatchContext);
 
 	const handleClickEmote = emotion => {
 		setEmotion(emotion);
@@ -69,16 +69,36 @@ const DiaryEditor = () => {
 			return;
 		}
 
+		if (
+			window.confirm(
+				isEdit ? '일기를 수정하시겠습니까?' : '새로운 일기를 작성하시겠습니까?',
+			)
+		) {
+			if (!isEdit) {
+				onCreate(date, content, emotion);
+			} else {
+				onEdit(originData.id, date, content, emotion);
+			}
+		}
 		console.log(date, content, emotion);
 		console.log(onCreate);
 		onCreate(date, content, emotion);
 		navigate('/', { replace: true });
 	};
 
+	useEffect(() => {
+		if (isEdit) {
+			setDate(getStringDate(new Date(parseInt(originData.date))));
+			setEmotion(originData.emotion);
+			setContent(originData.content);
+		} else {
+		}
+	}, [isEdit, originData]);
+
 	return (
 		<div className="DiaryEditor">
 			<MyHeader
-				headText={'새 일기쓰기'}
+				headText={isEdit ? '일기 수정하기' : '  새 일기쓰기'}
 				leftChild={
 					<MyButton text={'< 뒤로가기'} onClick={() => navigate(-1)} />
 				}
