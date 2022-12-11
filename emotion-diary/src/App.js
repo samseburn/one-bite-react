a974873614eb69c8e152bbad228153e93b33a4a8
samseburn <90seok_0@naver.com>
2022-12-11 21:50:00 +0900

    React 실전 - 배포 준비 :: 배포 전 오류 수정

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -56,7 +56,10 @@ function App() {
 			const diaryList = JSON.parse(localData).sort(
 				(a, b) => parseInt(b.id) - parseInt(a.id),
 			);
-			dataId.current = parseInt(diaryList[0].id) + 1;
+
+			if (diaryList.length >= 1) {
+				dataId.current = parseInt(diaryList[0].id) + 1;
+			}
 
 			console.log(diaryList);
 			console.log(dataId);
